d7c865b2cfdd8d37f2dcbc8321e3736f28d3c666
bcnichols3 <brian.nichols@vaynermedia.com>
2018-03-15 17:44:57 -0400

    yet another build

--- build/injectCSS.js
+++ build/injectCSS.js
@@ -5,8 +5,7 @@ var version = require('../package').version;
 function injectCSS() {
 	var css = document.createElement('link'); // Creates <style></style>
 	css.type = 'text/css'; // Specifies the type
-	// css.href = `https://unpkg.com/react-formian@${version}/build/style.css`;
-	css.href = '../../build/style.css';
+	css.href = 'https://unpkg.com/react-formian@' + version + '/build/style.css';
 	css.rel = 'stylesheet';
 	document.getElementsByTagName('head')[0].appendChild(css);
 }
