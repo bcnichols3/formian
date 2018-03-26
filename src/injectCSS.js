5a9d21f825150d5f9e39a44a1e86a9728f454f22
bcnichols3 <brian.nichols@vaynermedia.com>
2018-03-26 15:21:18 -0400

    rebuilding css

--- src/injectCSS.js
+++ src/injectCSS.js
@@ -6,8 +6,8 @@ export default function injectCSS() {
 	if (once) return;
 	const css = document.createElement('link');
 	css.type = 'text/css';
-	// css.href = `https://unpkg.com/react-formian@${version}/build/style.css`;
-	css.href = `../../build/style.css`;
+	css.href = `https://unpkg.com/react-formian@${version}/build/style.css`;
+	// css.href = `../../build/style.css`;
 	css.rel = 'stylesheet';
 	document.getElementsByTagName('head')[0].appendChild(css);
 	once = true;
