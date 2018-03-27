abfad0161fefbcf1c72293ee975f61258f82201f
bcnichols3 <brian.nichols@vaynermedia.com>
2018-03-27 12:37:14 -0400

    fixing css versioning issue

--- src/injectCSS.js
+++ src/injectCSS.js
@@ -1,12 +1,10 @@
-const version = require('../package').version;
-
 export default function injectCSS() {
 	let styles = document.getElementById('react-formian-default-styles');
 	if (styles) return;
 	const css = document.createElement('link');
 	css.id = 'react-formian-default-styles';
 	css.type = 'text/css';
-	css.href = `https://unpkg.com/react-formian@${version}/build/style.css`;
+	css.href = `https://unpkg.com/react-formian@0.2.*/build/style.css`;
 	// css.href = `../../build/style.css`;
 	css.rel = 'stylesheet';
 	document.getElementsByTagName('head')[0].appendChild(css);
